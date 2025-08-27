516da5028f3cdb9305f32244da82ca4a18300f0e
techsync01 <tech.syncbit@gmail.com>
2025-08-27 10:37:07 +0530

    adding absent icon properly

--- app/src/main/java/com/example/myapplication/uis/components/Home/AbsentAvatarList.kt
+++ app/src/main/java/com/example/myapplication/uis/components/Home/AbsentAvatarList.kt
@@ -1,32 +1,59 @@
 package com.example.myapplication.uis.components.Home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun AbsentAvatarList(avatarList: List<Int>) {
-    Row (
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(vertical = 12.dp)
+
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        avatarList.forEach { resId ->
-            Image(
-                painter = painterResource(id = resId),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(40.dp)
-                    .padding(end = 8.dp)
-                    .clip(CircleShape)
-            )
+        // Overlapping avatars
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy((-12).dp) // negative spacing for overlap
+        ) {
+            avatarList.take(3).forEach { resId ->
+                Image(
+                    painter = painterResource(id = resId),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(28.dp)
+                        .clip(RoundedCornerShape(50))
+                        .border(2.dp, Color.White, RoundedCornerShape(50))
+                )
+            }
         }
+
+        Spacer(Modifier.width(8.dp)) // spacing before "+More" text
+
+        // "+More" text if there are more avatars than shown
+
+            Text(
+                text = "+9 More",
+                style = MaterialTheme.typography.bodySmall
+            )
+
     }
+
+
 }
